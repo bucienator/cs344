13d0dd37761f2086b13d7f2e1e3d5e36f194769f
Peter BOLLA <peter.bolla@metacortex.hu>
2013-03-23 22:48:46 +0100

    HW4 solution

--- HW4/student_func.cu
+++ HW4/student_func.cu
@@ -31,10 +31,10 @@
    1) Histogram of the number of occurrences of each digit
    2) Exclusive Prefix Sum of Histogram
    3) Determine relative offset of each digit
-        For example [0 0 1 1 0 0 1]
-                ->  [0 1 0 1 2 3 2]
+		For example [0 0 1 1 0 0 1]
+				->  [0 1 0 1 2 3 2]
    4) Combine the results of steps 2 & 3 to determine the final
-      output location for each element and move it there
+	  output location for each element and move it there
 
    LSB Radix sort is an out-of-place sort and you will need to ping-pong values
    between the input and output buffers we have provided.  Make sure the final
@@ -43,12 +43,161 @@
 
  */
 
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
+
+__global__
+void exclusive_scan_add_carry(unsigned int * const d_data, const size_t numElems, const unsigned int * const d_carry)
+{
+	const size_t fullIdx = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int * const blockStart = d_data + blockIdx.x * blockDim.x;
+
+	if(fullIdx < numElems)
+		blockStart[threadIdx.x] += d_carry[blockIdx.x];
+}
+
+
+__global__
+void exclusive_scan_block(unsigned int * const d_data, const size_t numElems, unsigned int * const d_carry)
+{
+	extern __shared__ unsigned int s_block[];
+
+	const size_t fullIdx = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int * const blockStart = d_data + blockIdx.x * blockDim.x;
+
+	if(fullIdx < numElems)
+		s_block[threadIdx.x] = blockStart[threadIdx.x];
+	else
+		s_block[threadIdx.x] = 0;
+
+	if(threadIdx.x == blockDim.x-1) {
+		d_carry[blockIdx.x] = s_block[threadIdx.x];
+	}
+
+	__syncthreads();
+
+	int step = 1;
+
+	while(step < blockDim.x) {
+		if(threadIdx.x % (step * 2) == 0 && (threadIdx.x + step) < blockDim.x) {
+			s_block[blockDim.x - 1 - threadIdx.x] += s_block[blockDim.x - 1 - threadIdx.x - step];
+		}
+		step *= 2;
+
+		__syncthreads();
+	}
+
+	s_block[blockDim.x - 1] = 0;
+	__syncthreads();
+
+	do {
+		step /= 2;
+		if(threadIdx.x % (step * 2) == 0 && (threadIdx.x + step) < blockDim.x) {
+			unsigned int temp = s_block[blockDim.x - 1 - threadIdx.x];
+			s_block[blockDim.x - 1 - threadIdx.x] += s_block[blockDim.x - 1 - threadIdx.x - step];
+			s_block[blockDim.x - 1 - threadIdx.x - step] = temp;
+		}
+		__syncthreads();
+	} while(step > 1);
+
+	if(fullIdx < numElems)
+		blockStart[threadIdx.x] = s_block[threadIdx.x];
+
+	if(threadIdx.x == blockDim.x-1) {
+		d_carry[blockIdx.x] += s_block[threadIdx.x];
+	}
+
+}
+
+
+void exclusive_scan(unsigned int * d_data, const size_t numElems)
+{
+	const dim3 blockSize (1024, 1, 1);
+	const dim3 gridSize (numElems/blockSize.x + ((numElems%blockSize.x) == 0 ? 0 : 1), 1, 1);
+
+	// allocate carry
+	unsigned int * d_carry;
+	checkCudaErrors(cudaMalloc(&d_carry, gridSize.x * sizeof(unsigned int)));
+
+	// scan blocks
+	exclusive_scan_block<<<gridSize, blockSize, blockSize.x*sizeof(unsigned int)>>>(d_data, numElems, d_carry);
+	//gpuErrchk( cudaPeekAtLastError() );
+	//gpuErrchk( cudaDeviceSynchronize() );
+
+	// recursive scan carry
+	if(gridSize.x > 1) {
+		exclusive_scan(d_carry, gridSize.x);
+
+		// add carry
+		exclusive_scan_add_carry<<<gridSize, blockSize>>>(d_data, numElems, d_carry);
+		//gpuErrchk( cudaPeekAtLastError() );
+		//gpuErrchk( cudaDeviceSynchronize() );
+	}
+
+
+	// deallocate carry
+	checkCudaErrors(cudaFree(d_carry));
+
+}
+
+
+__global__
+void radix_sort_assign(const unsigned int* const d_inputVals,
+			   unsigned int* const d_selectors,
+			   const size_t numElems,
+			   const unsigned int blockMask,
+			   const unsigned int baseBit)
+{
+	size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if(idx < numElems) {
+		for(int i = 0; i < blockMask+1; i++) {
+			d_selectors[i * numElems + idx] = 0;
+		}
+		unsigned int block = d_inputVals[idx];
+		block = (block >> baseBit) & blockMask;
+		d_selectors[block * numElems + idx] = 1;
+	}
+}
+
+
+__global__
+void radix_sort_scatter(const unsigned int* const d_inputVals,
+					   const unsigned int* const d_inputPos,
+					   unsigned int* const d_outputVals,
+					   unsigned int* const d_outputPos,
+					   const size_t numElems,
+					   const unsigned int * const d_selectors,
+					   const unsigned int blockMask,
+					   const unsigned int baseBit)
+{
+	size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if(idx < numElems) {
+		unsigned int block = d_inputVals[idx];
+		block = (block >> baseBit) & blockMask;
+		unsigned int pos = d_selectors[block * numElems + idx];
+
+		d_outputVals[pos] = d_inputVals[idx];
+		d_outputPos[pos] = d_inputPos[idx];
+	}
+
+}
+
 
 void your_sort(unsigned int* const d_inputVals,
-               unsigned int* const d_inputPos,
-               unsigned int* const d_outputVals,
-               unsigned int* const d_outputPos,
-               const size_t numElems)
+			   unsigned int* const d_inputPos,
+			   unsigned int* const d_outputVals,
+			   unsigned int* const d_outputPos,
+			   const size_t numElems)
 {
   /****************************************************************************
   * You can use the code below to help with debugging, but make sure to       *
@@ -64,23 +213,84 @@ void your_sort(unsigned int* const d_inputVals,
   *                                                                           *
   * Thrust containers are used for copying memory from the GPU                *
   * ************************************************************************* */
-  
-  /* thrust::host_vector<unsigned int> h_inputVals(thrust::device_ptr<unsigned int>(d_inputVals),
-                                                thrust::device_ptr<unsigned int>(d_inputVals) + numElems);
+  /*
+  thrust::host_vector<unsigned int> h_inputVals(thrust::device_ptr<unsigned int>(d_inputVals),
+												thrust::device_ptr<unsigned int>(d_inputVals) + numElems);
   thrust::host_vector<unsigned int> h_inputPos(thrust::device_ptr<unsigned int>(d_inputPos),
-                                               thrust::device_ptr<unsigned int>(d_inputPos) + numElems);
+											   thrust::device_ptr<unsigned int>(d_inputPos) + numElems);
 
   thrust::host_vector<unsigned int> h_outputVals(numElems);
   thrust::host_vector<unsigned int> h_outputPos(numElems);
 
   reference_calculation(&h_inputVals[0], &h_inputPos[0],
-                        &h_outputVals[0], &h_outputPos[0],
-                        numElems);
+						&h_outputVals[0], &h_outputPos[0],
+						numElems);
   */
-   
+	const unsigned int baseBitIncrement = 4;
+	const unsigned int selectorMask = (1 << baseBitIncrement) - 1;
 
-  //TODO
-  //PUT YOUR SORT HERE
+	unsigned int * d_selectors;
+	checkCudaErrors(cudaMalloc(&d_selectors, (selectorMask+1) * numElems * sizeof(unsigned int)));
+  
+	const dim3 blockSizeAssign (1024, 1, 1);
+	const dim3 gridSizeAssign (numElems/blockSizeAssign.x + ((numElems%blockSizeAssign.x) == 0 ? 0 : 1), 1, 1);
+
+	unsigned int * values[] = {d_inputVals, d_outputVals};
+	unsigned int * pos[] = {d_inputPos, d_outputPos};
+	int target = 1;
+	for(unsigned int baseBit = 0; baseBit < sizeof(unsigned int) * 8; baseBit += baseBitIncrement) {
+		radix_sort_assign<<<gridSizeAssign,blockSizeAssign>>>(values[1-target], d_selectors, numElems, selectorMask, baseBit);
+		//gpuErrchk( cudaPeekAtLastError() );
+		//gpuErrchk( cudaDeviceSynchronize() );
+		/*{
+			thrust::host_vector<unsigned int> h_test(thrust::device_ptr<unsigned int>(d_selectors), thrust::device_ptr<unsigned int>(d_selectors + (selectorMask+1) * numElems));
+			unsigned int accu = 0;
+			size_t elemCnt = h_test.size();
+			for(size_t i = 0; i < h_test.size(); i++) {
+				unsigned int h = h_test[i];
+				if(h_test[i]!= 0 && h_test[i] != 1) {
+					printf("test error 3");
+					exit(1);
+				}
+				accu += h_test[i];
+			}
+			if(accu != numElems) {
+					printf("test error 4");
+					exit(1);
+			}
+		}*/
+
+		exclusive_scan(d_selectors, (selectorMask+1) * numElems);
+		/*{
+			thrust::host_vector<unsigned int> h_test(thrust::device_ptr<unsigned int>(d_selectors), thrust::device_ptr<unsigned int>(d_selectors + (selectorMask+1) * numElems));
+			unsigned int lastValue = 0;
+			for(size_t i = 0; i < h_test.size(); i++) {
+				if(lastValue > h_test[i]) {
+					printf("test error");
+					exit(1);
+				}
+				lastValue = h_test[i];
+			}
+			if(lastValue != numElems-1 && lastValue != numElems) {
+					printf("test error 2");
+					exit(1);
+			}
+		}*/
+
+		radix_sort_scatter<<<gridSizeAssign,blockSizeAssign>>>(values[1-target], pos[1-target], values[target], pos[target], numElems, d_selectors, selectorMask, baseBit);
+		//gpuErrchk( cudaPeekAtLastError() );
+		//gpuErrchk( cudaDeviceSynchronize() );
+
+		target = 1-target;
+	}
+
+	if(target != 0) {
+		// need to copy data
+		checkCudaErrors(cudaMemcpy(d_outputVals, d_inputVals, numElems * sizeof(unsigned int), cudaMemcpyDeviceToDevice));
+		checkCudaErrors(cudaMemcpy(d_outputPos, d_inputPos, numElems * sizeof(unsigned int), cudaMemcpyDeviceToDevice));
+	}
+
+	checkCudaErrors(cudaFree(d_selectors));
 
   /* *********************************************************************** *
    * Uncomment the code below to do the correctness checking between your    *
@@ -89,9 +299,9 @@ void your_sort(unsigned int* const d_inputVals,
 
   /*
   thrust::host_vector<unsigned int> h_yourOutputVals(thrust::device_ptr<unsigned int>(d_outputVals),
-                                                     thrust::device_ptr<unsigned int>(d_outputVals) + numElems);
+													 thrust::device_ptr<unsigned int>(d_outputVals) + numElems);
   thrust::host_vector<unsigned int> h_yourOutputPos(thrust::device_ptr<unsigned int>(d_outputPos),
-                                                    thrust::device_ptr<unsigned int>(d_outputPos) + numElems);
+													thrust::device_ptr<unsigned int>(d_outputPos) + numElems);
 
   checkResultsExact(&h_outputVals[0], &h_yourOutputVals[0], numElems);
   checkResultsExact(&h_outputPos[0], &h_yourOutputPos[0], numElems);
